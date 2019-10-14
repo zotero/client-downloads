7b220b60108514baa0d45de29aadabf8e72db4fe
Dan Stillman <dstillman@zotero.org>
2019-10-14 01:39:26 -0400

    Bump setup32.exe cut-off to 5.0.77

--- test/dl_test.js
+++ test/dl_test.js
@@ -67,16 +67,16 @@ describe("dl.php", function () {
 			assert.match(result, /client\/release\/5\.0\.[\d]+\/Zotero-5\.0\.[\d]+_linux-x86_64.tar.bz2$/);
 		});
 		
-		it("should offer setup.exe for win32 5.0.75", async function () {
+		it("should offer setup.exe for win32 5.0.76", async function () {
 			var result = await req(
-				url + '?channel=release&platform=win32&version=5.0.75'
+				url + '?channel=release&platform=win32&version=5.0.76'
 			);
 			assert.match(result, /client\/release\/5\.0\.[\d]+\/Zotero-5\.0\.[\d]+_setup.exe$/);
 		});
 		
-		it("should offer setup32.exe for win32 5.0.76", async function () {
+		it("should offer setup32.exe for win32 5.0.77", async function () {
 			var result = await req(
-				url + '?channel=release&platform=win32&version=5.0.76'
+				url + '?channel=release&platform=win32&version=5.0.77'
 			);
 			assert.match(result, /client\/release\/5\.0\.[\d]+\/Zotero-5\.0\.[\d]+_setup32.exe$/);
 		});
